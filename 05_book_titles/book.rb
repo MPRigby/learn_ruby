55bbfe005c30ab8ef0d2828271792164ca6e6726
MPRigby <markpringlerigby@gmail.com>
2017-10-27 13:47:43 -0400

    Finished fifth problem

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(new_title)
+    small_words = ['a', 'an', 'or', 'in', 'the', 'and', 'over', 'of']
+    words = new_title.downcase.split(" ")
+    words = [words[0].capitalize] + words[1..-1].map do |word|
+      if small_words.include? word
+        word
+      else
+        word.capitalize
+      end
+    end
+    @title = words.join(' ')
+  end
 end
